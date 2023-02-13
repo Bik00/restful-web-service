0f4e2b6ff16c790cd4cde00403b0586060d97553
bikoo <bikoo3002@naver.com>
2023-02-13 20:25:06 +0900

    Hello World Internationalized 설정 (다국어 지원 설정)

--- src/main/java/com/example/restfulwebservice/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/controller/HelloWorldController.java
@@ -1,12 +1,24 @@
 package com.example.restfulwebservice.helloworld.controller;
 
 import com.example.restfulwebservice.helloworld.bean.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+//    public HelloWorldController(MessageSource messageSource) {
+//        this.messageSource = messageSource;
+//    }
 
     // GET
     // /hello-World (endpoint)
@@ -24,5 +36,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variables/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable  String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String HelloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language", required=false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
