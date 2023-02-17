089ae32aa4fb657c7ec73fd77f8706d9c20f1b9c
bikoo <bikoo3002@naver.com>
2023-02-17 20:03:56 +0900

    Spring Java Persistence API (JPA) 세팅 & H2 데이터베이스 설정

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -3,15 +3,34 @@ package com.example.restfulwebservice.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+
+        // form login 해제
+        // httpSecurity.formLogin().disable();
+
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers("/**").hasRole("USER").and().formLogin();
+
+
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+
+        return httpSecurity.build();
+    }
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() throws Exception {
