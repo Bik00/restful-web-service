9f17612d3839cc436bab63e681339c8bafa93f62
bikoo <bikoo3002@naver.com>
2023-02-17 20:54:24 +0900

    data.sql 생성 & H2 Database Initialization 설정

--- src/main/java/com/example/restfulwebservice/user/bean/User.java
+++ src/main/java/com/example/restfulwebservice/user/bean/User.java
@@ -3,22 +3,29 @@ package com.example.restfulwebservice.user.bean;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.Date;
 
+//@JsonIgnoreProperties(value={"password"})
+//@JsonFilter("UserInfo")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//@JsonIgnoreProperties(value={"password"})
-//@JsonFilter("UserInfo")
 @Schema(description = "템플릿 관련 VO")
+@Entity
+@Table(name = "USERS")
 public class User {
 
+    @Id
+    @GeneratedValue
     @Schema(description = "사용자 ID")
     private Integer id;
 
