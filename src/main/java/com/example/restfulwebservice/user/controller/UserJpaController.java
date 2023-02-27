1944959322b64493af691cad63819d2b4a3ce386
bikoo <bikoo3002@naver.com>
2023-02-27 22:01:18 +0900

    JPA - Delete, Create 생성

--- src/main/java/com/example/restfulwebservice/user/controller/UserJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/controller/UserJpaController.java
@@ -5,13 +5,15 @@ import com.example.restfulwebservice.user.bean.User;
 import com.example.restfulwebservice.user.repository.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -42,5 +44,21 @@ public class UserJpaController {
         model.add(linkTo.withRel("/users/{id}"));
 
         return model;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into users(id, join_date, name, password, ssn) values(1, now(), 'Bikoo', 'pass1', '123456-111111');
-insert into users(id, join_date, name, password, ssn) values(2, now(), 'Alice', 'pass2', '123456-222222');
-insert into users(id, join_date, name, password, ssn) values(3, now(), 'Steven', 'pass3', '123456-333333');
+insert into users(id, join_date, name, password, ssn) values(9001, now(), 'Bikoo', 'pass1', '123456-111111');
+insert into users(id, join_date, name, password, ssn) values(9002, now(), 'Alice', 'pass2', '123456-222222');
+insert into users(id, join_date, name, password, ssn) values(9003, now(), 'Steven', 'pass3', '123456-333333');
 
